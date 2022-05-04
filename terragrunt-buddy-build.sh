5b3ea1f314c272e783402c8c038cd07dc12a1cc9
Sebastian Zavadschi <szavadschi@gmail.com>
2022-05-04 18:14:27 -0400

    Add TerragruntBuddy files

---
+++ terragrunt-buddy-build.sh
@@ -0,0 +1,15 @@
+[ -d bin ]            || mkdir bin
+[ -d tmp ]            || mkdir tmp
+[ -f vault.zip ]      || wget -O tmp/vault.zip      https://releases.hashicorp.com/vault/1.10.2/vault_1.10.2_linux_amd64.zip
+[ -f terraform.zip ]  || wget -O tmp/terraform.zip  https://releases.hashicorp.com/terraform/1.0.3/terraform_1.0.3_linux_amd64.zip
+[ -f terragrunt.zip ] || wget -O bin/terragrunt     https://github.com/gruntwork-io/terragrunt/releases/download/v0.36.6/terragrunt_linux_amd64 
+
+cd tmp
+unzip vault.zip && mv vault ../bin
+unzip terraform.zip && mv terraform ../bin
+
+cd ../
+
+docker build -t devopsmd/terragruntbuddy:tf-1.0.3-tg-0.36.6 -f aws-linux.Dockerfile .
+
+rm -f bin/* tmp/*
---
+++ terragrunt-buddy.Dockerfile
@@ -0,0 +1,5 @@
+FROM amazon/aws-cli:2.6.1
+
+ADD bin/* /usr/local/bin/
+
+RUN yum clean metadata && yum install -y git jq 
