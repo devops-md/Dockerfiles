817a2c5c536dbf4cc9a497e0fef36739b0aa7ba8
Sebastian Zavadschi <szavadschi@gmail.com>
2022-12-21 11:19:29 -0500

    Multiple fixes - Add openssl - Add edocker user - Fix yum clean - Update build script - add .gitignore

--- terragrunt-buddy-build.sh
+++ terragrunt-buddy-build.sh
@@ -1,8 +1,17 @@
+DEFAULT_TERRAFORM_VERSION="1.0.3"
+DEFAULT_TERRAGRUNT_VERSION="0.38.7"
+DEFAULT_VAULT_VERSION="1.10.2"
+
+
+[[ -z "$TERRAFORM_VERSION" ]]  && TERRAFORM_VERSION=${EFAULT_TERRAFORM_VERSION}
+[[ -z "$TERRAGRUNT_VERSION" ]] && TERRAGRUNT_VERSION=${DEFAULT_TERRAGRUNT_VERSION}
+[[ -z "$VAULT_VERSION" ]]      && VAULT_VERSION=${DEFAULT_VAULT_VERSION}
+
 [ -d bin ]            || mkdir bin
 [ -d tmp ]            || mkdir tmp
-[ -f vault.zip ]      || wget -O tmp/vault.zip      https://releases.hashicorp.com/vault/1.10.2/vault_1.10.2_linux_amd64.zip
-[ -f terraform.zip ]  || wget -O tmp/terraform.zip  https://releases.hashicorp.com/terraform/1.0.3/terraform_1.0.3_linux_amd64.zip
-[ -f terragrunt.zip ] || wget -O bin/terragrunt     https://github.com/gruntwork-io/terragrunt/releases/download/v0.36.6/terragrunt_linux_amd64 
+[ -f vault.zip ]      || wget -O tmp/vault.zip      https://releases.hashicorp.com/vault/${VAULT_VERSION}/vault_${VAULT_VERSION}_linux_amd64.zip
+[ -f terraform.zip ]  || wget -O tmp/terraform.zip  https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+[ -f terragrunt.zip ] || wget -O bin/terragrunt     https://github.com/gruntwork-io/terragrunt/releases/download/v${TERRAGRUNT_VERSION}/terragrunt_linux_amd64 
 
 cd tmp
 unzip vault.zip && mv vault ../bin
--- terragrunt-buddy.Dockerfile
+++ terragrunt-buddy.Dockerfile
@@ -2,7 +2,8 @@ FROM amazon/aws-cli:2.6.1
 
 ADD bin/* /usr/local/bin/
 
-RUN yum clean metadata && yum install -y git jq 
+RUN yum install -q -y git jq openssl && yum clean metadata && adduser edocker
 
+USER edocker
 WORKDIR /app
 ENTRYPOINT []
