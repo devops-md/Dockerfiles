413c0e57fcd33b3955e3aece3c113a560c0d52e5
Sebastian Zavadschi <sz@devops.md>
2023-10-16 12:50:19 -0400

    Multiple updates * Update AWS Cli base image to latest * Update Vault to lates * Add Go Lang * Some fixes in build shell script

--- terragrunt-buddy-build.sh
+++ terragrunt-buddy-build.sh
@@ -1,24 +1,25 @@
-DEFAULT_TERRAFORM_VERSION="1.0.3"
-DEFAULT_TERRAGRUNT_VERSION="0.38.7"
-DEFAULT_VAULT_VERSION="1.10.2"
+DEFAULT_TERRAFORM_VERSION="1.5.3"
+DEFAULT_TERRAGRUNT_VERSION="0.48.4"
+DEFAULT_VAULT_VERSION="1.15.0"
 
 
-[[ -z "$TERRAFORM_VERSION" ]]  && TERRAFORM_VERSION=${EFAULT_TERRAFORM_VERSION}
+[[ -z "$TERRAFORM_VERSION" ]]  && TERRAFORM_VERSION=${DEFAULT_TERRAFORM_VERSION}
 [[ -z "$TERRAGRUNT_VERSION" ]] && TERRAGRUNT_VERSION=${DEFAULT_TERRAGRUNT_VERSION}
 [[ -z "$VAULT_VERSION" ]]      && VAULT_VERSION=${DEFAULT_VAULT_VERSION}
 
-[ -d bin ]            || mkdir bin
-[ -d tmp ]            || mkdir tmp
-[ -f vault.zip ]      || wget -O tmp/vault.zip      https://releases.hashicorp.com/vault/${VAULT_VERSION}/vault_${VAULT_VERSION}_linux_amd64.zip
-[ -f terraform.zip ]  || wget -O tmp/terraform.zip  https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
-[ -f terragrunt.zip ] || wget -O bin/terragrunt     https://github.com/gruntwork-io/terragrunt/releases/download/v${TERRAGRUNT_VERSION}/terragrunt_linux_amd64 
+[ -d bin ]           || mkdir bin
+[ -d tmp ]           || mkdir tmp
+[ -f bin/terraform ] || wget -O tmp/terraform.zip  https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+[ -f bin/terragrunt] || wget -O bin/terragrunt     https://github.com/gruntwork-io/terragrunt/releases/download/v${TERRAGRUNT_VERSION}/terragrunt_linux_amd64 
+[ -f bin/vault ]     || wget -O tmp/vault.zip      https://releases.hashicorp.com/vault/${VAULT_VERSION}/vault_${VAULT_VERSION}_linux_amd64.zip
 
-cd tmp
-unzip vault.zip && mv vault ../bin
-unzip terraform.zip && mv terraform ../bin
+#cd tmp
+unzip tmp/terraform.zip -d bin/
+unzip tmp/vault.zip -d bin/
 
-cd ../
+#cd ../
+chmod +x bin/*
 
-docker build -t devopsmd/terragruntbuddy:tf-1.0.3-tg-0.36.6 -f terragrunt-buddy.Dockerfile .
+docker build -t devopsmd/terragruntbuddy:tf-${TERRAFORM_VERSION}-tg-${TERRAGRUNT_VERSION} -f terragrunt-buddy.Dockerfile .
 
-rm -f bin/* tmp/*
+#rm -f tmp/*
--- terragrunt-buddy.Dockerfile
+++ terragrunt-buddy.Dockerfile
@@ -1,8 +1,12 @@
-FROM amazon/aws-cli:2.6.1
+FROM amazon/aws-cli:2.13.26
 
 ADD bin/* /usr/local/bin/
 
-RUN yum install -q -y git jq openssl && yum clean metadata && adduser edocker
+RUN yum update -y && \
+    yum upgrade -y &&\
+    yum install -q -y golang git jq openssl &&\
+    yum clean metadata &&\
+    adduser edocker
 
 USER edocker
 WORKDIR /app
